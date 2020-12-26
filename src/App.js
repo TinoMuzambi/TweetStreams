ad848e9353d7ffe4c07214211a8227e3a0e4a48f
Tino <tinomuzambi@gmail.com>
2020-12-26 18:21:57 +0200

    Adds use of state for tweets.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
 import "./App.css";
 import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
 
 function App() {
-	const tweets = ["", "", "", "", "", "", ""];
-	const socket = io("https://live-tweet-stream.herokuapp.com/", {
-		origins: "https://live-tweet-stream.herokuapp.com/",
+	const [tweets, setTweets] = useState([]);
+	const socket = io("http://localhost:5000", {
+		origins: "http://localhost:5000",
 	});
 
-	socket.on("connect", () => {
-		console.log("Connected to server.");
-	});
+	useEffect(() => {
+		socket.on("connect", () => {
+			console.log("Connected to server.");
+		});
 
-	socket.on("tweet", (tweet) => {
-		console.log(tweet);
-	});
+		socket.on("tweet", (tweet) => {
+			setTweets(...tweets, tweet);
+			console.log(tweets);
+		});
+	}, []);
+
 	return (
 		<div className="content">
 			<h1 className="title">Tweet Stream</h1>
 
-			{tweets.map((tweet, key) => (
-				<div className="card" key={key}>
-					<div className="author">
-						<img
-							src="/assets/twitter.png"
-							alt="Twitter logo"
-							className="twitter"
-						/>
-						<h3 className="author-title">Author</h3>
+			{tweets &&
+				tweets.map((tweet, key) => (
+					<div className="card" key={key}>
+						<div className="author">
+							<img
+								src="/assets/twitter.png"
+								alt="Twitter logo"
+								className="twitter"
+							/>
+							<h3 className="author-title">
+								@{tweet.includes.users[0].username}
+							</h3>
+						</div>
+						<p className="text">{tweet.data.text}</p>
+						<img src="/logo192.png" alt="tweet" className="tweet-image" />
+						<a
+							href={`https://twitter.com/${tweet.includes.users[0].username}/status/${tweet.data.id}`}
+							className="link"
+						>
+							Go to Tweet
+						</a>
 					</div>
-					<p className="text">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-						doloribus repellat eveniet qui quia ex beatae temporibus dolores
-						dolorum nemo.
-					</p>
-					<img src="/logo192.png" alt="tweet" className="tweet-image" />
-					<a href="https://twitter.com" className="link">
-						Go to Tweet
-					</a>
-				</div>
-			))}
+				))}
 		</div>
 	);
 }
