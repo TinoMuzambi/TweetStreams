1e807ea7566b8ea8aaa2503c1890f55e337216ca
Tino <tinomuzambi@gmail.com>
2020-12-26 19:40:22 +0200

    Removes useeffect.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { io } from "socket.io-client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
 	const [tweets, setTweets] = useState([]);
@@ -8,44 +8,43 @@ function App() {
 		origins: "http://localhost:5000",
 	});
 
-	useEffect(() => {
-		socket.on("connect", () => {
-			console.log("Connected to server.");
-		});
+	socket.on("connect", () => {
+		console.log("Connected to server.");
+	});
 
-		socket.on("tweet", (tweet) => {
-			setTweets(...tweets, tweet);
-			console.log(tweets);
-		});
-	}, []);
-
+	socket.on("tweet", (tweet) => {
+		const currTweets = tweets;
+		currTweets.unshift(tweet);
+		setTweets(currTweets);
+		console.log(tweets);
+	});
 	return (
 		<div className="content">
 			<h1 className="title">Tweet Stream</h1>
 
-			{tweets &&
-				tweets.map((tweet, key) => (
-					<div className="card" key={key}>
-						<div className="author">
-							<img
-								src="/assets/twitter.png"
-								alt="Twitter logo"
-								className="twitter"
-							/>
-							<h3 className="author-title">
-								@{tweet.includes.users[0].username}
-							</h3>
-						</div>
-						<p className="text">{tweet.data.text}</p>
-						<img src="/logo192.png" alt="tweet" className="tweet-image" />
-						<a
-							href={`https://twitter.com/${tweet.includes.users[0].username}/status/${tweet.data.id}`}
-							className="link"
-						>
-							Go to Tweet
-						</a>
+			{tweets.map((tweet, key) => (
+				<div className="card" key={key}>
+					<div className="author">
+						<img
+							src="/assets/twitter.png"
+							alt="Twitter logo"
+							className="twitter"
+						/>
+						<h3 className="author-title">
+							@{tweet.includes.users[0].username}
+						</h3>
 					</div>
-				))}
+					<p className="text">{tweet.data.text}</p>
+					<img src="/logo192.png" alt="tweet" className="tweet-image" />
+					<a
+						href={`https://twitter.com/${tweet.includes.users[0].username}/status/${tweet.data.id}`}
+						className="link"
+						target="__blank"
+					>
+						Go to Tweet
+					</a>
+				</div>
+			))}
 		</div>
 	);
 }
