0df9d8296a2951568d1f76f3e115a748b7fef405
Tino <tinomuzambi@gmail.com>
2020-12-26 20:57:24 +0200

    Updates styling.

--- src/App.js
+++ src/App.js
@@ -27,29 +27,31 @@ function App() {
 				<h1 className="title">Tweet Stream</h1>
 			</nav>
 
-			{tweets.map((tweet, key) => (
-				<div className="card" key={key}>
-					<div className="author">
-						<img
-							src="/assets/twitter.png"
-							alt="Twitter logo"
-							className="twitter"
-						/>
-						<h3 className="author-title">
-							@{tweet.includes.users[0].username}
-						</h3>
+			<div className="wrapper">
+				{tweets.map((tweet, key) => (
+					<div className="card" key={key}>
+						<div className="author">
+							<img
+								src="/assets/twitter.png"
+								alt="Twitter logo"
+								className="twitter"
+							/>
+							<h3 className="author-title">
+								@{tweet.includes.users[0].username}
+							</h3>
+						</div>
+						<p className="text">{tweet.data.text}</p>
+						<img src="/logo192.png" alt="tweet" className="tweet-image" />
+						<a
+							href={`https://twitter.com/${tweet.includes.users[0].username}/status/${tweet.data.id}`}
+							className="link"
+							target="__blank"
+						>
+							Go to Tweet
+						</a>
 					</div>
-					<p className="text">{tweet.data.text}</p>
-					<img src="/logo192.png" alt="tweet" className="tweet-image" />
-					<a
-						href={`https://twitter.com/${tweet.includes.users[0].username}/status/${tweet.data.id}`}
-						className="link"
-						target="__blank"
-					>
-						Go to Tweet
-					</a>
-				</div>
-			))}
+				))}
+			</div>
 		</div>
 	);
 }
